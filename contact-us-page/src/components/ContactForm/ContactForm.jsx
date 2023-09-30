bd8d06cda164062373dc3d36ce7a8c0eaad9cd6a
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-09-30 23:23:11 +0530

    added form jsx

--- contact-us-page/src/components/ContactForm/ContactForm.jsx
+++ contact-us-page/src/components/ContactForm/ContactForm.jsx
@@ -5,15 +5,49 @@ import { IoMdCall } from "react-icons/io";
 import { HiMail } from "react-icons/hi";
 
 const ContactForm = () => {
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log("Name : ",event.target[0].value);
+        console.log("Email : ",event.target[1].value);
+        console.log("Text : ",event.target[2].value);
+
+    }
+
+    const onViaCall = () => console.log("VIA CALL CLICKED!");
+    const onViaChat = () => console.log("VIA CHAT CLICKED!");
+    const onViaEmail = () => console.log("VIA EMAIL CLICKED!");
+
     return <section className={styles.container}>
     <div className={styles.contact_form}>
         <div className={styles.top_btn}>
-            <Button text = "VIA SUPPORT CHAT" icon = {<MdMessage fontSize="20px"/>} />
-            <Button text = "VIA CALL" icon = {<IoMdCall fontSize="20px"/>} />
+            <Button onClick={onViaChat} text = "VIA SUPPORT CHAT" icon = {<MdMessage fontSize="20px"/>} />
+            <Button onClick={onViaCall} text = "VIA CALL" icon = {<IoMdCall fontSize="20px"/>} />
         </div>
-        <Button isOutline={true} text = "VIA EMAIL FORM" icon = {<HiMail fontSize="20px"/>} />
+        <Button onClick={onViaEmail} isOutline={true} text = "VIA EMAIL FORM" icon = {<HiMail fontSize="20px"/>} />
+
+        <form  onSubmit={onSubmit} >
+            <div className={styles.form_control}>
+                <label htmlFor="name">Name</label>
+                <input type='text' name='name' />
+            </div>
+            <div className={styles.form_control}>
+                <label htmlFor="name">Email</label>
+                <input type='email' name='email' />
+            </div>
+            <div className={styles.form_control}>
+                <label htmlFor="text">Text</label>
+                <textarea name='text' rows='5'/>
+            </div>
+            <div style={{display: "flex", justifyContent: "end"}}>
+                <Button text = "SUBMIT" />
+            </div>
+        </form>
+
     </div>
-    <div className={styles.contact_image}></div>
+    <div className={styles.contact_image}>
+        <img src='/images/contact.svg' alt='Contact Image' />
+    </div>
     </section>
 }
 
